7fc57a3353717b515c75f99099860d597028e7d0
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-02-24 09:18:41 +0100

    Added projekt

---
+++ DCS/DCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
